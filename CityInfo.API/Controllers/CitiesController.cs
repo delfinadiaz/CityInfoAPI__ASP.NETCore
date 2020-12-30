b21cad9a8aff209e6d2fccd7fa751ca14a1c2d42
delfina <delfii.sb@gmail.com>
2020-12-30 16:03:22 -0300

    Agregar archivos de proyecto.

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using CityInfo.API.Models;
+using CityInfo.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        private readonly ICityInfoRepository _cityInfoRepository;
+        private readonly IMapper _mapper;
+
+        public CitiesController(ICityInfoRepository cityInfoRepository, IMapper mapper)
+        {
+            _cityInfoRepository = cityInfoRepository ??
+                throw new ArgumentNullException(nameof(cityInfoRepository));
+            _mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet]
+        public IActionResult GetCities()
+        {
+            var cityEntities = _cityInfoRepository.GetCities();
+
+            //var results = new List<CityWithoutPointsOfInterestDTO>();
+
+            //foreach (var cityEntity in cityEntities)
+            //{
+            //    results.Add(new CityWithoutPointsOfInterestDTO
+            //    {
+            //        Id = cityEntity.Id,
+            //        Description = cityEntity.Description,
+            //        Name = cityEntity.Name
+            //    });
+            //}
+
+            return Ok(_mapper.Map<IEnumerable<CityWithoutPointsOfInterestDTO>>(cityEntities));
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id, bool includePointsOfInterest = false)
+        {
+            var city = _cityInfoRepository.GetCity(id, includePointsOfInterest);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            if (includePointsOfInterest)
+            {
+                return Ok(_mapper.Map<CityDTO>(city));
+            }
+
+            return Ok(_mapper.Map<CityWithoutPointsOfInterestDTO>(city));
+        }
+    }
+}
---
+++ CityInfo.API/Profiles/PointOfInterestProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Profiles
+{
+    public class PointOfInterestProfile : Profile
+    {
+        public PointOfInterestProfile()
+        {
+            CreateMap<Entities.PointOfInterest, Models.PointOfInterestDTO>();
+            CreateMap<Models.PointOfInterestForCreationDTO, Entities.PointOfInterest>();
+            CreateMap<Models.PointOfInterestForUpdateDTO, Entities.PointOfInterest>()
+                .ReverseMap();
+
+        }
+    }
+}
